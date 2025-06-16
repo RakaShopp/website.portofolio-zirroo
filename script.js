216c8044c7fae6499cc5e07cc4962a4c8c706cb3
RakaShopp <raka.shopp4@gmail.com>
2025-06-17 01:09:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,13 @@
     player.play();
   }
 </script>
+const faders = document.querySelectorAll('.fade-in');
+const appearOptions = { threshold: 0.1 };
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add('show');
+    observer.unobserve(entry.target);
+  });
+}, appearOptions);
+faders.forEach(fader => appearOnScroll.observe(fader));
