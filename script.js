48bd96d5c075f5b8d3bc63c576bdcfe425d20e19
RakaShopp <raka.shopp4@gmail.com>
2025-06-17 01:10:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-<script>
-  function playSong(url) {
-    const player = document.getElementById("audioPlayer");
-    player.src = url;
-    player.play();
-  }
-</script>
 const faders = document.querySelectorAll('.fade-in');
 const appearOptions = { threshold: 0.1 };
 const appearOnScroll = new IntersectionObserver(function(entries, observer) {
